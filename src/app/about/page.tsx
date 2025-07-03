085cf6353bd1288b7cec2cf43b78c08c32b56ec5
Milton D Diaz Molina <MiltonD.Diaz@gmail.com>
2025-07-03 20:31:36 +0000

    now shows a picture but not my  picture, use this one

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
         <div className="h-32 bg-gradient-to-r from-primary/10 to-accent/10" />
         <CardHeader className="items-center text-center -mt-16">
           <Avatar className="h-32 w-32 border-4 border-background shadow-md">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/135089?v=4" alt="Milton Diaz" />
+              <AvatarImage src="https://firebasestudio.com/api/files/view/clwkx5v0k0001dc082pazg0s4" alt="Milton Diaz" />
               <AvatarFallback>MD</AvatarFallback>
           </Avatar>
           <div className="pt-4">
