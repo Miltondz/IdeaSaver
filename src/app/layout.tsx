e521a60b7f0106927f655b15b885c065a2082057
Milton D Diaz Molina <MiltonD.Diaz@gmail.com>
2025-07-05 01:00:56 +0000

    Hay manera de mejorar la velocidad general de la app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from 'next';
+import { PT_Sans } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AppShell } from '@/components/AppShell';
@@ -14,19 +15,25 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
 };
 
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+  variable: '--font-pt-sans'
+});
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={ptSans.variable}>
       <head>
         <meta name="theme-color" content="#6f29ef" />
         <link rel="apple-touch-icon" href="/icon.svg"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         <AuthProvider>
